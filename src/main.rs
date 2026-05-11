a3a01d1080db6b7cc1b2caf52d620f19c7bebbba
Gordon Lin <contact@gordonlin.ca>
2026-05-11 13:15:35 -0400

    Created config for primary and replicas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub primary_url: String,
+    pub replica_url: String,
+    pub listen_addr: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
+}
+
+impl Config {
+    fn from_env() -> Result<Self> {
+        Ok(Self {
+            listen_addr: std::env::var("LISTEN_ADDR")
+                .unwrap_or_else(|_| "0.0.0.0:6432".to_string()),
+            primary_url: std::env::var("PRIMARY_URL")
+                .unwrap_or_else(|_| "postgres://localhost:5432/postgres".to_string()),
+            replica_url: std::env::var("REPLICA_URL")
+                .unwrap_or_else(|_| "postgres://localhost:5432/postgres".to_string()),
+        })
+    }
 }
